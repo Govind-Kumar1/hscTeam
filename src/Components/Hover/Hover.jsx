01f0fa77655af1e2500abc03c5e8f82690cbed00
web_dev <sukritprakash2020@gmail,com>
2024-02-16 17:01:25 +0530

    new commit

--- src/Components/Hover/Hover.jsx
+++ src/Components/Hover/Hover.jsx
@@ -4,10 +4,14 @@ import  img1 from '../Assets/img.jpg'
 const Hover = (props) => {
   return (
     <div>
-       <div className="card__collection clear-fix">
+      
+       <div  className="card__collection clear-fix">
 <div className="cards cards--two">
+    
     <img src={img1} className="img-responsive" alt="Cards "/>
-    <span className="cards--two__rect"></span>
+    <span className="cards--two__rect">
+    
+    </span>
     <span className="cards--two__tri"></span>
     <p>{props.name}<br/></p>
     
