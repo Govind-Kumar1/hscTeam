4445d532618e35ffde795b63b157dcc22009d841
Govind-Kumar1 <136782602+Govind-Kumar1@users.noreply.github.com>
2024-02-18 18:36:43 +0530

    commit

--- src/Components/Hover/Hover.jsx
+++ src/Components/Hover/Hover.jsx
@@ -24,22 +24,7 @@ const Hover = (props) => {
       <li><i className="fab fa-linkedin-in"></i></li>
     </ul>
   </div>
-  {/* <div className="cards cards--three">
-    <img src={img1}className="img-responsive" alt=""/>
-    <span className="cards--three__rect-1">
-      <span className="shadow-1"></span>
-      <p>Chris Levnon</p>
-    </span>
-    <span className="cards--three__rect-2">
-      <span className="shadow-2"></span>
-    </span>
-    <span className="cards--three__circle"></span>
-    <ul className="cards--three__list">
-      <li><i className="fab fa-facebook-f"></i></li>
-      <li><i className="fab fa-twitter"></i></li>
-      <li><i className="fab fa-linkedin-in"></i></li>
-    </ul>
-  </div>*/}
+  
 </div>
     </div> 
   )
